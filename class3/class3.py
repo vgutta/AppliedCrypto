daa9c5f887a3a83a7d8124f9cb53b0a3169481f5
Vineeth Gutta <vineethgutta@gmail.com>
2019-02-27 15:41:12 +0000

    init

---
+++ class3/class3.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+int("Andy".encode('hex'), 16)
+_ ^ 320945240
+_ ^ 320945240
+format(_, 'x')
+('0' * (len(_) % 2)) + _
+_.decode('hex')
+"andy" ^ "banana"
+from pwn import *
+%save 1-100 class3.py
+%save -r class3.py 1-99
